152ddfdd7c7cf5e4e8dc1ecb6a06f5f6dca27c60
Zainul <zainulbasharnazmin@gmail.com>
2023-06-19 13:49:13 +0530

    first-commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol>
+                <li key= "relativeListItem1">Nani</li>
+                 <li key= "relativeListItem2">Mami</li>
+                 <li key= "relativeListItem3">Uncle</li>
+                 <li key= "relativeListItem4">Masi</li>
+               </ol>
     </div>
   )
 }
